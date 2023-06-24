24cd38bbc4233a10295a638b3ad1fd2c9ce29ff3
User <kurokforeva@yandex.ru>
2023-06-24 19:32:16 +0500

    Добавлен блок по парсингу

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 TOKEN = '5551101530:AAH-W0AePOM19cyB70J47gsG8670Cargxsc'
 AVITO_SEARCH = 'https://www.avito.ru/ekaterinburg?q='
 AVITO_SEARCH2 = 'https://www.avito.ru/ekaterinburg?p=2&q='
+MOVIE_SEARCH = 'https://www.youtube.com/results?search_query='
--- main.py
+++ main.py
@@ -1,62 +1,75 @@
 import time
-
 import telebot
 from selenium import webdriver
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import config
 from telebot import types
+
 
 bot = telebot.TeleBot(config.TOKEN)
 
 keyb = types.ReplyKeyboardMarkup(resize_keyboard=True)
 button1 = types.KeyboardButton('Найти среднюю цену товара')
-keyb.add(button1)
+button2 = types.KeyboardButton('Поиск видео')
+keyb.add(button1, button2)
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id,'Доброго времени суток, ' + message.from_user.first_name + '. '
-                                      'Этот бот предназначен для поиска средней цены товара на Авито',reply_markup=keyb)
+    bot.send_message(message.chat.id, 'Доброго времени суток, ' + message.from_user.first_name + '. '
+                        'Этот бот предназначен для поиска средней цены товара на Авито',
+                     reply_markup=keyb)
+
 
 @bot.message_handler(content_types=['text'])
 def text(message):
     if message.text == 'Найти среднюю цену товара':
-        bot.send_message(message.chat.id,'Введите название',reply_markup=types.ReplyKeyboardRemove())
-        bot.register_next_step_handler(message,search)
+        bot.send_message(message.chat.id, 'Введите название', reply_markup=types.ReplyKeyboardRemove())
+        bot.register_next_step_handler(message, search)
+    if message.text == 'Поиск видео':
+        bot.send_message(message.chat.id, 'Введите название\n'
+                                          'Примечание: Данный бот выводит только первые пять видео',
+                         reply_markup=types.ReplyKeyboardRemove())
+        bot.register_next_step_handler(message, movie_search)
 
 
 def search(message):
-    bot.send_message(message.chat.id,'Выполняю поиск')
-    driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+    bot.send_message(message.chat.id, 'Выполняю поиск')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(config.AVITO_SEARCH + message.text)
 
     page_source = driver.page_source
-    soup = BeautifulSoup(page_source,'html.parser')
-    products = soup.find_all('meta',attrs={'itemprop':'price'})
+    soup = BeautifulSoup(page_source, 'html.parser')
+    products = soup.find_all('meta', attrs={'itemprop': 'price'})
     driver.get(config.AVITO_SEARCH2 + message.text)
     products2 = soup.find_all('meta', attrs={'itemprop': 'price'})
     sum = 0
     for product in products2:
         sum += int(product.get('content'))
     for product in products:
-        sum+=int(product.get('content'))
-    avg = round(sum/(len(products)+len(products2)),2)
-    bot.send_message(message.chat.id,'Средняя стоимость ' + message.text + ' : ' + str(avg) + '₽',reply_markup=keyb)
+        sum += int(product.get('content'))
+    avg = round(sum / (len(products) + len(products2)), 2)
+    bot.send_message(message.chat.id, 'Средняя стоимость ' + message.text + ' : ' + str(avg) + ' ₽', reply_markup=keyb)
 
 
+def movie_search(message):
+    bot.send_message(message.chat.id, 'Идет поиск')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(config.MOVIE_SEARCH + message.text)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    movies = soup.find_all(name='a', attrs={'id': 'video-title'})
+    time.sleep(4)
+    counter = 0
+    for movie in movies:
+        if counter == 5: break
+        bot.send_message(message.chat.id,'https://www.youtube.com/' + movie.get('href'),
+                         reply_markup=keyb)
+        counter += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    bot.send_message(message.chat.id,'Поиск закончен')
 
 if __name__ == '__main__':
     bot.polling()
